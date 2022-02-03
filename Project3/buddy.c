753ccff25cb6b0396c192a20ca2341a03a80ea2b
jasonkboise <jasonkuphaldt@u.boisestate.edu>
2022-02-03 14:56:56 -0700

    Added a bunch of notes

--- Project3/buddy.c
+++ Project3/buddy.c
@@ -5,16 +5,56 @@
 #include "buddy.h"
 
 int buddy_init(void) { 
+	//when you initalize, you will start with just one big chunk of memory of DEFAULT_MEM_SIZE,
+	//and initialize a lot of empty lists, which will hold smaller chunks
+	//then split it when the user uses buddy_malloc for a smaller size than the large chunk
+
+	/*
+	base = sbrk();
+	
+	int i;
+	for (i = 0; i< 29; i++) {
+		next <- & avai[i];
+		prev <- & avai[i];
+		kval <- i;
+		tag <- -1;
+	}
+	*/
 	return FALSE;
 }
 
 void *buddy_malloc(size_t size)
 {
+	//first add the size of the header to the amount they want to allocate
+	//int realsize = (char *)size + 24 (maybe?)
+	//this is ACTUALLY what you are trying to allocate
+
+	//start searching from lgsize
+	/*
+	for (i = lgsize; i <29; i++) {
+		if list not empty (then there a block free to use!)
+			break;
+	}
+	while (j > lgsize) {
+		...
+		return something;
+	}
 	return NULL;
+	*/
+
+	//everytime you split the big chunk, you need to cast to add a buddy header
+	
+
+	return NULL;
+	//pointer = (char *)pointer + 24 (the size of the header)
+	//return pointer
+
 }
 
 void buddy_free(void *ptr) 
 {
+	//first do pointer = (char *)ptr - 24;
+	//then start freeing from pointer
 }
 
 void printBuddyLists(void)
