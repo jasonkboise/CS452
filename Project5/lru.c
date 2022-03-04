63cef528c1547568fca539a780abe6aed147a22b
cs452 <cs452@xyno.boisestate.edu>
2022-03-03 21:01:28 -0700

    get() is finished?

--- Project5/lru.c
+++ Project5/lru.c
@@ -24,6 +24,49 @@ LRUCache* LRUCacheCreate(int capacity) {
 }
 
 int LRUCacheGet(LRUCache* obj, int key) {
+    if (obj->hash[key] == NULL || obj->size == 0) {
+        return -1;
+    }
+    else {
+        //hash[key] is at front of list, needs to go to back
+        if (obj->head == obj->hash[key]) {
+            //move head pointer to next node
+            obj->head = obj->hash[key]->next;
+            //make new head's prev point to NULL
+            obj->hash[key]->next->prev = NULL;
+            //make the tail's next point to hash[key]
+            obj->tail->next = obj->hash[key];
+            //make hash[key]'s prev point to the current tail
+            obj->hash[key]->prev = obj->tail;
+            //make hash[key] the new tail
+            obj->tail = obj->hash[key];
+            //make hash[key]'s next point to NULL
+            obj->hash[key]->next = NULL;
+
+        }
+        //hash[key] is already at the end of the list, don't need to move it
+        else if (obj->tail == obj->hash[key]){
+
+        }
+        //hash[key] is in the middle of the list, move to the back
+        else {
+            //make the node before hash[key]'s next point to the one after hash[key]
+            obj->hash[key]->prev->next = obj->hash[key]->next;
+            //make the node after hash[key]'s prev point to the one before hash[key]
+            obj->hash[key]->next->prev = obj->hash[key]->prev;
+            //make the tail's next point to hash[key]
+            obj->tail->next = obj->hash[key];
+            //make hash[key]'s prev point to the tail
+            obj->hash[key]->prev = obj->tail;
+            //make hash[key]'s next point to NULL
+            obj->hash[key]->next = NULL;
+            //make hash[key] the new tail
+            obj->tail = obj->hash[key];
+        }
+        //return the value of hash[key]
+        return obj->hash[key]->value;
+    }
+    
 }
 
 void LRUCachePut(LRUCache* obj, int key, int value) {
